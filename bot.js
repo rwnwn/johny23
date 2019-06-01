3ea6c3c0d4e512c80ca2d85998179dce04347f0f
rwnwn <45011018+rwnwn@users.noreply.github.com>
2019-06-01 08:25:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("489322902886547457");
+const mohamed= member.guild.channels.get("547519337360130048");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**__Welcome To  başarı __☆.**`), 4000)        
